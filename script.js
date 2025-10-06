906ac44542d73a10d2d3d6dfc2ec027012c0d8df
GUSTAVO DE PAULA SILVA <25132581@CFP127ALUNO.LOCAL>
2025-10-06 15:51:09 -0300

    css concluido

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+apiKey = '8bb9f4bfef3260d4276216738392a45a'
+url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR`
+
+async function getMovieList() {
+    
+}
