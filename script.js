d4fea7bbd5a359a25e8f70ce9477c0984f248001
GUSTAVO DE PAULA SILVA <25132581@CFP127ALUNO.LOCAL>
2025-10-06 16:57:25 -0300

    js em indo

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 'use strict'
 
-apiKey = '8bb9f4bfef3260d4276216738392a45a'
-url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR`
+const apiKey = '8bb9f4bfef3260d4276216738392a45a'
+
+const page = 
+
 
 async function getMovieList() {
-    
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&page=${page}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
 }
+
+getMovieList()
+// criarPoster()
