f12707437a2c245ebc43bf75613e09c0e36bd590
Gustavo <gustavodepaulasilva299@gmail.com>
2025-10-25 13:39:05 -0300

    home concluida

--- script.js
+++ script.js
@@ -16,45 +16,56 @@ async function getMovieList() {
 async function createMovieList() {
     const images = await getMovieList()
 
-    
-    images.results.forEach(item =>{
+
+    images.results.forEach(item => {
 
         const img = document.createElement('img')
 
         const container = document.getElementById('main_index')
         const container2 = document.createElement('div')
         const container3 = document.createElement('div')
+
         img.src = `https://image.tmdb.org/t/p/w500/${item['poster_path']}`
+
+        const nome = document.createElement("h3")
+        nome.textContent = `${item['title']}`
+        nome.classList.add("subtible")
+
         container2.appendChild(img)
+        container2.appendChild(nome)
+
+
         container.appendChild(container2)
+
         container2.classList.add('img_container')
         container3.classList.add('main_container')
+
     })
 
 }
 
 
 const btn = document.getElementById('proximo_button')
-btn.addEventListener('click', async function(){
+btn.addEventListener('click', async function () {
     page = page + 1
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&page=${page}`
     const response = await fetch(url)
     const data = await response.json()
     console.log(data)
     const container = document.getElementById('main_index')
-    container.textContent=''
+    container.textContent = ''
     createMovieList()
 })
 
 const btn2 = document.getElementById('anterior_button')
-btn2.addEventListener('click', async function(){
+btn2.addEventListener('click', async function () {
     page = page - 1
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&page=${page}`
     const response = await fetch(url)
     const data = await response.json()
     console.log(data)
     const container = document.getElementById('main_index')
-    container.textContent=''
+    container.textContent = ''
     createMovieList()
 })
 
