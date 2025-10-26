88d6e27f8e9c43af3b8a2b8fa4eb10fa0ba70fd3
Gustavo <gustavodepaulasilva299@gmail.com>
2025-10-26 10:32:43 -0300

    pesquisa concluida

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const apiKey = '8bb9f4bfef3260d4276216738392a45a'
 
 let page = 1
-
+let film_input = document.getElementById("film_input")
 
 async function getMovieList() {
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&page=${page}`
@@ -13,6 +13,13 @@ async function getMovieList() {
     return data
 }
 
+async function getSearchlist() {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${film_input.value}&api_key=${apiKey}&language=pt-BR&page=1`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+}
 
 async function createMovieList() {
     const images = await getMovieList()
@@ -49,8 +56,8 @@ async function createMovieList() {
             const data = await response.json()
             console.log(data)
 
-            const img2 = document.createElement('img')
-            img2.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+            const img = document.createElement('img')
+            img.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
 
             const titulo = document.createElement("h2")
             const sinopse = document.createElement("h3")
@@ -61,7 +68,7 @@ async function createMovieList() {
             sinopse.textContent = `${data['overview']}`
 
             tempo_filme.textContent = `${data['runtime']} minutos`
-            avaliacao_filme.textContent = `${data['vote_average']} %`
+            avaliacao_filme.textContent = `${parseInt(data['vote_average'])} estrelas`
 
             const container_hero = document.createElement('div')
             const container_bonus = document.createElement('div')
@@ -88,7 +95,86 @@ async function createMovieList() {
     })
 
 }
+async function createSearchList() {
+    const images = await getSearchlist()
 
+
+    images.results.forEach(item => {
+
+        const img = document.createElement('img')
+
+        const container = document.getElementById('main_index')
+        const container2 = document.createElement('div')
+        const container3 = document.createElement('div')
+
+        img.src = `https://image.tmdb.org/t/p/w500/${item['poster_path']}`
+
+        const nome = document.createElement("h3")
+        nome.textContent = `${item['title']}`
+        nome.classList.add("subtible")
+
+        container2.appendChild(img)
+        container2.appendChild(nome)
+        container.appendChild(container2)
+
+        container2.classList.add('img_container')
+        container3.classList.add('main_container')
+
+        container2.addEventListener('click', async function () {
+            const id_fime = item.id
+            const container = document.getElementById('main_index')
+            container.textContent = ''
+
+            const url = `https://api.themoviedb.org/3/movie/${id_fime}?api_key=${apiKey}&language=pt-BR`
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+
+            const img = document.createElement('img')
+            img.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+
+            const titulo = document.createElement("h2")
+            const sinopse = document.createElement("h3")
+            const tempo_filme = document.createElement("h4")
+            const avaliacao_filme = document.createElement("span")
+
+            titulo.textContent = `${data['title']}`
+            sinopse.textContent = `${data['overview']}`
+
+            tempo_filme.textContent = `${data['runtime']} minutos`
+            avaliacao_filme.textContent = `${parseInt(data['vote_average'])} estrelas`
+
+            const container_hero = document.createElement('div')
+            const container_bonus = document.createElement('div')
+            const container1 = document.createElement('div')
+
+            titulo.classList.add("titulo")
+            sinopse.classList.add("sinopse")
+            tempo_filme.classList.add("informacao_bonus")
+            avaliacao_filme.classList.add("informacao_bonus")
+            container1.classList.add("container")
+            container_hero.classList.add("container_hero")
+            container_bonus.classList.add("container_bonus")
+
+            container_hero.appendChild(titulo)
+            container_hero.appendChild(sinopse)
+            container_bonus.appendChild(tempo_filme)
+            container_bonus.appendChild(avaliacao_filme)
+
+            container1.append(img)
+            container_hero.append(container_bonus)
+            container1.appendChild(container_hero)
+            container.appendChild(container1)
+        })
+    })
+}
+
+const btn_pesquisar = document.getElementById("btn_pesquisar")
+btn_pesquisar.addEventListener("click", async function () {
+    const container = document.getElementById('main_index')
+    container.textContent = ''
+    createSearchList()
+})
 
 const btn = document.getElementById('proximo_button')
 btn.addEventListener('click', async function () {
@@ -117,6 +203,10 @@ logo.addEventListener('click', async function () {
     container.textContent = ''
     createMovieList()
 })
+
+
+
+
 
 createMovieList()
 // getMovieList()
